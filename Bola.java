2ea4e206a1c931d751f4fbd97d75f39a7241f7a5
Migguel12 <128193333+Migguel12@users.noreply.github.com>
2023-05-05 21:24:17 -0300

    Add files via upload

---
+++ Bola.java
@@ -0,0 +1,14 @@
+public class Bola {
+
+    private String cor;
+    private double diametro;
+    private String material;
+
+    public void trocaCor(String novaCor){
+        cor = novaCor;
+
+    }
+    public void mostraCor(){
+        return cor;
+    }
+}
